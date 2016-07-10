d3cbb76a44baeba9e5f238cffa8088288b24196c
AndyL <missedmemo@gmail.com>
2016-07-09 20:40:59 -0700

    attempt functional implementation of data translation tree

--- js/tree.js
+++ js/tree.js
@@ -10,13 +10,36 @@ Tree.prototype.addNode = function( data ) {
 };
 
 Tree.prototype.insertNode = function( data, comparator ) {
-  var target = _.find( this.children, comparator );
+  var predicate = comparator(data);
+  console.log( 'predicate:', predicate );
+  var target = _.find( this.children, predicate );
   if( target ) {
     target.addNode( data );
   }
   else {
     this.addNode(data);
   }
+};
+
+Tree.prototype.toString = function( offset ) {
+
+  var spaces = offset || '';
+  var string = spaces + 'I am node: '   + this.value.name
+                            + '(pid:'   + this.value.pid
+                            + ', ppid:' + this.value.ppid
+                            + '), and ';
+
+  if( this.children.length === 0 ) {
+    string += 'I am childless\n';
+  } else {
+    string += 'these are my kids:\n';
+
+    this.children.forEach( function(child) {
+      string += child.toString( spaces + '   ' );
+    });
+  }
+
+  return string;
 };
 
 /*
@@ -28,15 +51,18 @@ Tree.prototype.insertNode = function( data, comparator ) {
 ]
 */
 
+var nodeMatch = function( newNodeData ) {
+  return function( existingNodeData ) {
+    return newNodeData.ppid === existingNodeData.pid;
+  };
+};
+
 var tree = new Tree( { name: 'explorer.exe', pid: 1, ppid: 5 } );
 
-tree.insertNode( { name: 'cmd.exe', pid: 2, ppid: 1 }, function(node) {
-  console.log( 'name:', name );
-  console.log( 'pid:', pid );
-  console.log( 'ppid:', ppid );
-  return node.ppid === pid;
-});
-console.log( 'tree:', tree, tree.children[0] );
+tree.insertNode( { name: 'cmd.exe', pid: 2, ppid: 1 }, nodeMatch );
+tree.insertNode( { name: 'python.exe', pid: 3, ppid: 2 }, nodeMatch );
+
+console.log( tree.toString() );
 
 
 
